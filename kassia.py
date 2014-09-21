7d5511450e95d9506b48ed823e4d99aa68c3f12e
John Daly <johnmdaly@gmail.com>
2014-09-21 01:21:56 -0400

    continued linebreak work, changed default font to 20pt

--- kassia.py
+++ kassia.py
@@ -7,6 +7,7 @@ from reportlab.lib.pagesizes import letter
 
 import sys
 import xml.etree.ElementTree as ET
+import re
 
 class Cursor:
     def __init__(self,x=0,y=0):
@@ -66,7 +67,7 @@ class Kassia:
             nPos = self.linebreak(neumesText,lyricsText)
             
             for glyph in nPos:
-                c.setFont("EZPsaltica",24)
+                c.setFont("EZPsaltica",20)
                 c.drawString(glyph[0]+self.leftmargin,glyph[1] + 600,glyph[2])
                 print glyph
                 
@@ -80,27 +81,37 @@ class Kassia:
     def linebreak(self,neumes,lyrics = None):
         """Break neumes and lyrics into lines"""
         cr = Cursor(0,0)
+        lyricArray = re.split(' ',lyrics)
         # If lyric spans multiple neumes
         #   then see if lyric is wider than span
         #   else see if width of glypch is max of neume and lyric
+        charSpace = 4 # default space between characters
+        textOffset = 30 # default space lyrics appear below neumes
         neumeArray = neume_dict.translate(neumes).split(' ')
         neumePos = []
+        lyrIdx = 0
         for neume in neumeArray:
             #print("Neume length: " + str(pdfmetrics.stringWidth(neume,'EZPsaltica',24)))
-            nWidth = pdfmetrics.stringWidth(neume,'EZPsaltica',24)
+            nWidth = pdfmetrics.stringWidth(neume,'EZPsaltica',20)
             if nWidth > 1.0: # if it's not a gorgon or other small symbol
                 if (nWidth + cr.x) >= self.lineWidth: # line break
                     cr.x, cr.y = 0, cr.y - self.lineHeight
+                    # does it take a lyric syllable?
                     neumePos.append((cr.x,cr.y,neume))
                 else: # no line break
+                    # does it take a lyric syllable?
                     neumePos.append((cr.x,cr.y,neume))
-                cr.x += nWidth
+                cr.x += nWidth + charSpace
             else:
-                # offset for gorgon
-                # offset for apli
-                neumePos.append((cr.x,cr.y,neume))
+                # offsets for gorgon
+                # offsets for apli
+                # offset for kentima
+                # offsets for omalon
+                # offsets for antikenoma
+                # offsets for eteron
+                neumePos.append((cr.x - charSpace,cr.y,neume))
             
-        print neumePos
+        #print neumePos
         return neumePos
         
 
