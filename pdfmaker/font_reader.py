e2c4a7582ce47b6ef07f629194a5f11830ba3719
Trevor Bullock <trevorjbullock@gmail.com>
2016-04-04 00:45:58 -0400

    Ability to read OTF fonts. Better error handling when reading in fonts

--- pdfmaker/font_reader.py
+++ pdfmaker/font_reader.py
@@ -1,22 +1,35 @@
 #!/usr/bin/python
 from reportlab.pdfbase import pdfmetrics
-from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.pdfbase.ttfonts import TTFont, TTFError
 from fontTools import ttLib
+from PIL import ImageFont
 from glob import glob
 
 
 def registerFonts():
     rootDir = "fonts"
 
-    for fontLoc in glob(rootDir + "/*.ttf"):
-        ttFont = ttLib.TTFont(fontLoc)
+    ftypes = ('/*.ttf', '/*.otf')
+    files_grabbed = []
+
+    # Get all font files in fonts directory
+    for ftype in ftypes:
+        temp = glob(rootDir + ftype)
+        files_grabbed.extend(temp)
+
+    # Try to register them
+    for fontLoc in files_grabbed:
+        fontName = ImageFont.truetype(fontLoc, 1)
         try:
-            fontName = shortName(ttFont)
-            #print "Registering {} from {}".format(fontName[0],fontLoc)
-            pdfmetrics.registerFont(TTFont(fontName[0],fontLoc))
-        except IOError as e:
-            print e
-    print "Registered Fonts"
+            pdfmetrics.registerFont(TTFont(fontName.font.family,fontLoc))
+        except TTFError as e:
+            print "Error: {}".format(e)
+            raise SystemExit
+
+    # Check that default fonts are registered
+    registeredFonts = pdfmetrics.getRegisteredFontNames()
+    if not "EZ Psaltica" in registeredFonts or not "EZ Omega" in registeredFonts:
+        print "Warning: Default fonts 'EZ Psaltica' and 'EZ Omega' are missing from the fonts directory"
 
 
 FONT_SPECIFIER_NAME_ID = 4
